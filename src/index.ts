586ac1d8922023dccb00d28f64377733b48cb314
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-07-13 03:11:53 -0300

    initial project structure

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const port = +process.env.PORT || 5000;
+
+app.listen(port, () => 
+    console.log(chalk.bold.green(`Server is up and running on port ${port}`))
+);
